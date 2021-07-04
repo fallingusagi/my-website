e9670866c0dd5e07a40e2d1a8ba36598d41abaf3
fallingusagi <tanlisarah@gmail.com>
2021-07-04 17:25:41 +0800

    Updated FR, PayLah

--- src/pages/projectFR.jsx
+++ src/pages/projectFR.jsx
@@ -38,22 +38,37 @@ export const ProjectPageFR = () => {
 
                     {/* Body */}
                     <div className="space-y-8">
+                        {/* Background */}
+                        <div className="space-y-4">
+                            <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
+                                {"Background"}
+                            </p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"To address the high manpower costs associated with maintaining a hotline, "}
+                                {"DBS decided to invest in an online platform to gather complaints and effectively "}
+                                {"automate this process, and so the Facilities Reporting Tool was born."}
+                            </p>
+                        </div>
                         {/* The situation */}
                         <div className="space-y-4">
                             <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
                                 {"The situation"}
                             </p>
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"When we first got to work on this project, faults reported through hotline calls "}
-                                {"far exceeded form submissions. Top users were mainly Personal Assistants or Admin staff "}
-                                {"who consolidated requests from others as part of their daily tasks. "}
-                                {"Project stakeholders felt the tool was under-utilised, "}
+                                {"When we came along, only a handful of users were using the online platform. "}
+                                {"The majority still preferred voicing their displeasures over the hotline, "}
+                                {"and the bank was bleeding money from maintaining both. "}
+                                {"The only users who seemed to use the platform regularly were Personal Assistants or Admin staff "}
+                                {"who had been delegated the task as part of their job scope, and had no choice but to comply. "}
+                            </p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"Project stakeholders wanted to understand why the tool was so under-utilised, "}
                                 {"and with some initial research, we quickly found out why. "}
                             </p>
                             <ul className="max-w-max space-y-0 list-decimal list-inside">
                                 <div className="space-y-0">
                                     <li className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                        {"Users couldn't find the right category"}
+                                        {"Way too many categories to choose from"}
                                     </li>
                                     {/* <div className="pl-5">
                                         <p className="text-xs sm:text-sm text-mytheme-ebony">
@@ -73,7 +88,7 @@ export const ProjectPageFR = () => {
                                 </div>
                                 <div className="space-y-0">
                                     <li className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                        {"Users couldn't track their existing requests"}
+                                        {"No easy way to track requests"}
                                     </li>
                                     {/* <div className="pl-5">
                                         <p className="text-xs sm:text-sm  text-mytheme-ebony">
@@ -86,45 +101,59 @@ export const ProjectPageFR = () => {
                         {/* Re-categorising the categories */}
                         <div className="space-y-4">
                             <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
-                                {"Re-categorising the categories"}</p>
+                                {"Simplifying decisions for the user"}</p>
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"While observing a couple of people using the system, we realised there might be a mental model mismatch. "}
+                                {"The fact that 4 out of 5 users had difficulty finding the right category to report a fault "}
+                                {"was actually not surprising when we found out there were over 170 sub-categories in total. "}</p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"Through our observation, we noticed there might also be a mental model mismatch issue. "}
                                 {"Though users tended to focus on the problem that needed to be solved, the categories presented to them "}
-                                {"were a mix of object types, locations, and solutions (e.g. plumbing). In addition, there were over "}
-                                {"170 sub-categories in total."}</p>
+                                {"were a mix of object types, locations, and solutions "}
+                                {"(e.g. a user might be thinking of 'leak', 'spill' or 'mess' but they needed to find and select 'mop'). "}</p>
                             <img src={FRcardsort} />
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"Our first course of action was to simplify the list and then do a simple card sort, "}
+                                {"Our first course of action was to simplify the list and then invite some users to join us in a card sorting exercise, "}
                                 {"wanting to see how users might intuitively group the sub-categories. "}
-                                {"Most behaved as expected, grouping primarily by object classifications."}</p>
+                                {"Most behaved as expected, grouping primarily by object types. So it seemed "}
+                                {"we could reduce the burden on users by restructuring the category choices based primarily on object types, "}
+                                {"instead of forcing users to think of the solutions they needed. It made sense - "}
+                                {"users describe the problem, the system prescribes the solution."}</p>
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"Then, using several common scenarios, we tested this hypothesis with "}
-                                {"some users, where there were two paths (one location-based, one object-based) "}
+                                {"To test our hypothesis, we presented users with several common scenarios "}
+                                {"describing a problem, where there were two paths (one object-first and one location-first) "}
                                 {"Interestingly, users didn't always pick the object-first path."}</p>
-                            <img src={FRpietree} />
+                            {/* <img src={FRpietree} /> */}
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"Upon closer examination, we confirmed with users that there "}
-                                {"was a caveat to our earlier hypothesis. Users preferred an object-first approach "}
+                                {"Upon closer examination, we saw there was a caveat to our earlier hypothesis. Users preferred an object-first approach "}
                                 {"when they could articulate the problem and it directly affected themselves (like the light above their workspace), "}
                                 {"but had no significant preference in shared spaces or when the exact cause of the issue was unknown (like the toilets or cafeteria)."}</p>
+                            <img src={FRpietree} />
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"Overall, we kept the object-first approach but also made it easier for users to report "}
-                                {"location-specific issues with handy QR codes in common areas. "}</p>
+                                {"Hence, we decided to keep the object-first approach but also made it easier for users to report "}
+                                {"location-specific issues with handy QR codes in common areas and points of need. "}
+                                {"The QR-code locations and placement were decided through another user survey, and we were "}
+                                {"able to pre-populate specific location information when the codes were scanned, so although "}
+                                {"users still had to describe the problem faced, they could already see their location displayed, "}
+                                {"and had less fields to fill."}</p>
                         </div>
-                        {/* Streamlining the form */}
+                        {/* Reducing user input required */}
                         <div className="space-y-4">
                             <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
-                                {"Streamlining the form"}</p>
+                                {"Reducing user input required"}</p>
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"To tackle the second issue, we did a comprehensive review of the existing form "}
-                                {"which had a lot of unecessary fields and was difficult to navigate (it had some really crazy dropdowns too)"}</p>
+                                {"To tackle the second issue of the lengthy form, we did a comprehensive review of the existing form "}
+                                {"which took users 9 minutes on average to get through. No doubt this was in part due to the "}
+                                {"category confusion from earlier, but the form also had a lot of unecessary fields and "}
+                                {"was difficult to navigate (it had some really crazy dropdowns too)"}</p>
                             <img src={FRcrazydropdowns} />
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"We worked out the back-end logic with our dev squad to help the system autopopulate "}
-                                {"employee and location information to reduce the number of fields required. "}</p>
+                                {"We worked out the back-end logic with our dev squad to help the system auto-populate "}
+                                {"readily available information from internal databases, including employee and "}
+                                {"default location information to reduce the effort required from the user. "}</p>
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"We also split up the steps to make it easy for users to tell us the What "}
-                                {"and the Where of the problem, as well as How to contact them, and ended up with 3 simple steps. "}</p>
+                                {"Based on our earlier finding, we also split up the form into steps to make the process "}
+                                {"more conversational and reflect the users' mental model, starting with the What (object-first) "}
+                                {"and the Where of the problem, then How to contact them, and ended up with 3 simple steps. "}</p>
                             <img src={FR2} />
                         </div>
                         {/* Timelier (and more accessible) updates */}
@@ -132,38 +161,42 @@ export const ProjectPageFR = () => {
                             <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
                                 {"Closing the loop"}</p>
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"Lastly, we wanted to improve the post-submission portion of the journey. "}
-                                {"So we tagged along with some of the staff who went from site to site and actually did the fixing "}
+                                {"Lastly, we wanted to improve the post-submission portion of the journey by addressing the "}
+                                {"'What happens now...?' bewilderment users felt after successfully making a request (in the event they actually did get through the form). "}
+                                {"We began by tagging along with some of the staff who went from site to site to fix faults "}
                                 {"to understand the people involved and actions taken behind the scenes, from recieving a request to fulfilling it."}</p>
                             <img src={FRjourneymap} />
                             <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"With this, we identified key points in the journey where the system could feasibly be alerted, "}
+                                {"With this, we identified key points in the journey that could feasibly be tracked, "}
                                 {"and sought to reflect this progress back to the requestor, in the same page where they "}
-                                {"submitted the request for easy tracking."}</p>
-                                <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                {"For some final touches, we designed some quick gratitude prompts into the feedback collection "}
-                                {"to encourage colleagues to remember to appreciate each other's work! With the increased visibilty "}
-                                {"of our solution, we hoped that users would also be more understanding and appreciative of the effort "}
-                                {"that goes into fixing workplace problems."}</p>
+                                {"submitted the request for easy access."}</p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"Overall, we were sure we had made substantial improvements to smoothen the user journey. But we wanted to "}
+                                {"take it a step further for a more empathetic and delightful experience. "}</p>
                             <img src={FR4} />
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"For some final touches, we designed some quick gratitude prompts into the feedback collection "}
+                                {"to encourage colleagues to remember to appreciate each other's work. "}
+                                {"With the increased visibility of our solution, we hoped that users would also be more understanding and appreciative of the effort "}
+                                {"that others put in to fix their problems - it's always better when we work together!"}</p>
                         </div>
                         {/* Results */}
                         <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
-                                {"Key results"}</p>
+                            {"Key results"}</p>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-12 flex items-stretch">
                             <div className="flex-col">
                                 <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
-                                    {"Faster process"}</p>
+                                    {"Faster form completion"}</p>
                                 <p className="font-bold text-lg sm:text-3xl md:text-4xl lg:text-5xl text-mytheme-charcoal">
-                                    {"~1.5 min"}</p>
+                                    {"6x faster"}</p>
                                 <p className="text-sm sm:text-base md:text-lg text-mytheme-beigepink">
-                                    {"vs 9 min today"} </p>
+                                    {"1.5 min vs 9 min today"} </p>
                             </div>
                             <div className="flex-col">
                                 <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
                                     {"Higher adoption"}</p>
                                 <p className="font-bold text-lg sm:text-3xl md:text-4xl lg:text-5xl text-mytheme-charcoal">
-                                    {"+100%"}</p>
+                                    {"2x users"}</p>
                                 <p className="text-sm sm:text-base md:text-lg text-mytheme-beigepink">
                                     {"vs today"} </p>
                             </div>
@@ -171,11 +204,11 @@ export const ProjectPageFR = () => {
                                 <p className="text-sm sm:text-base md:text-lg text-mytheme-beigepink">
                                     {"Less problematic requests"}</p>
                                 <p className="font-bold text-lg sm:text-3xl md:text-4xl lg:text-5xl text-mytheme-beigepink">
-                                    {"Coming soon"}</p>
+                                    {"WIP"}</p>
                                 <p className="text-sm sm:text-base md:text-lg text-mytheme-beigepink">
                                     {"vs 31% today"} </p>
                             </div>
-                            
+
                         </div>
 
                     </div>
--- src/pages/projectPayLah.jsx
+++ src/pages/projectPayLah.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ProjectPayLah from '../assets/ProjectPayLah.png';
-import PayLah1 from '../assets/PayLah1.png'
+import PayLah1 from '../assets/PayLah1.png';
+import PayLah2 from '../assets/PayLah2.png';
+import PayLah3 from '../assets/PayLah3.png';
+import PayLah4 from '../assets/PayLah4.png';
+import PayLah5 from '../assets/PayLah5.png';
+import PayLahinterview from '../assets/PayLahinterview.png';
+import PayLahcompetitors from '../assets/PayLahcompetitors.png';
+import PayLahideation from '../assets/PayLahideation.png';
 import { Header } from '../components/Header/Header';
 
 export const ProjectPagePayLah = () => {
@@ -10,33 +17,94 @@ export const ProjectPagePayLah = () => {
 
             <div className="px-12 sm:px-24 md:px-32 lg:px-48 xl:px-56 2xl:px-64 pt-8 pb-24 space-y-4 sm:space-y-8 justify-center w-full...">
 
-                <div className="grid grid-cols-1 space-y-16 place-self-center w-full...">
+                <div className="grid grid-cols-1 space-y-8 place-self-center w-full...">
                     <div className="h-1 w-full"></div>
                     <img src={ProjectPayLah} />
-                    <div className="xs:flex-col sm:inline-flex xs:space-x-0 sm:space-x-8">
-                        <div className="flex flex-col">
-                            <p className="pr-2 font-medium text-mytheme-charcoal">
-                                {"DBS PayLah!"}
-                            </p>
-                            <p className="text-sm text-mytheme-beigegray">
-                                {"app, android/iOS"}
-                            </p>
-                        </div>
-                        <div className="flex flex-col space-y-2">
-                            <p className="text-sm text-mytheme-ebony">
-                                {"DBS PayLah! is a mobile payment/wallet service offered by Singapore-based DBS Bank Ltd, the largest bank in SEA. The app allows users to get rides, order meals, book tickets and handle all kinds of everyday transactions including payment at over 180,000 acceptance points across Singapore. "}
-                            </p>
-                            <p className="text-sm text-mytheme-ebony">
-                                {"In 2019, PayLah! was still primarily a cashless P2P & C2B app, and usage statistics revealed that most users used the app infrequently, for single transactions. And thus came the question: How might we increase user engagement on PayLah! ?"}
-                            </p>
-                        </div>
+                    {/* Header */}
+                    <div className="space-y-4">
+                        <p className="text-xs sm:text-sm md:text-base font-light text-mytheme-beigepink">
+                            {"PayLah! / android and iOS app for DBS Bank Ltd."}
+                        </p>
+                        <p className="font-bold text-lg sm:text-3xl md:text-4xl lg:text-5xl text-mytheme-charcoal">
+                            {"How we helped a payments app go beyond just payments"}
+                        </p>
+                        <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                            {"DBS PayLah! is a mobile payment/wallet service offered by Singapore-based "}
+                            {"DBS Bank Ltd, the largest bank in SEA. "}
+                            {/* {"The app allows users to get rides, order meals, book tickets and handle all kinds of everyday transactions "}
+                            {"including payment at over 180,000 acceptance points across Singapore."} */}
+                        </p>
                     </div>
-                    <div>
-                        <img src={PayLah1} />
+                    {/* Body */}
+                    <div className="space-y-8">
+                        {/* The prompt */}
+                        <div className="space-y-4">
+                            <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
+                                {"The prompt"}</p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"When I joined the team, cashless payments were still picking up in popularity. "}
+                                {"Usage statistics revealed that only about a third of PayLah!'s users were active, "}
+                                {"most users used the app infrequently, "}
+                                {"and those that used it only used it to do one thing (pay friends, scan QR or for e-commerce). "}
+                                {"And thus came the question: How might we increase user engagement on PayLah! ?"}
+                            </p>
+                        </div>
+
+                        {/* The situation */}
+                        <div className="space-y-4">
+                            <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
+                                {"What do people want that we aren't giving them?"}
+                            </p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"To find our more about our target customers' behaviours and what they wanted "}
+                                {"from a payments app, I interviewed and surveyed a total of 54 milennials."}
+                            </p>
+                            <img src={PayLahinterview} />
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"As some patterns emerged (unfortunately I can't go into too much detail here for fear "}
+                                {"of a potential lawsuit coming my way), we were able to form distinct personas and identify "}
+                                {"the reasons we were losing them and where exactly they were dropping off in our current journey. "}
+                                {"We also gained some insights into the motivations behind choice of payment mode, "}
+                                {"which turned out to be closely related to rewards, and how each segment defined 'rewards'."}
+                            </p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"At the same time, I looked into our competitors' value propositions, business models, "}
+                                {"and features offered. These included both direct/indirect and local/global "}
+                                {"payment, deals, and rewards-based apps. "}
+                            </p>
+                            <img src={PayLahcompetitors} />
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"Our primary concerns included evaluating our performance against other payments-based apps, "}
+                                {"but also making the correct comparisons since we were expanding our market definitions. We were "}
+                                {"currently in the business of payments, but we fully intended to be in the business of "}
+                                {"rewards as well."}
+                            </p>
+                        </div>
+
+                        {/* Ideation */}
+                        <div className="space-y-4">
+                            <p className="text-base sm:text-lg md:text-xl font-semibold text-mytheme-charcoal">
+                                {"How can we make PayLah! more rewarding?"}</p>
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"I was given plenty of freedom to explore various concepts, and it was nice to let loose "}
+                                {"and go a little crazy with some initial ideas. I went from conceptualising to refining "}
+                                {"and iterating, first with wireframes and then hi-fi screens. I also fleshed out the user "}
+                                {"journeys for several concepts and created interactive prototypes to demonstrate how they would work."}
+                            </p>
+                            <img src={PayLahideation} />
+                            <p className="text-sm sm:text-base md:text-lg text-mytheme-ebony">
+                                {"Eventually I distilled these into 5 distinct concepts, each targeting a "}
+                                {"specific problem identified in our earlier research. Again avoiding a lawsuit here, "}
+                                {"but feel free to reach out if you'd like to know more about any project :)"}
+                            </p>
+                            <img src={PayLah1} />
+                            <img src={PayLah2} />
+                            <img src={PayLah3} />
+                            <img src={PayLah4} />
+                            <img src={PayLah5} />
+                        </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     );
